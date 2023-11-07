bfface386e4625fae31dbdc9e3b89ab034030261
Daniel Bradley <daniel@pulumi.com>
2023-11-07 17:18:35 +0100

    Add substack demo

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
 import * as pulumi from "@pulumi/pulumi";
+import { SubStack } from "@pulumi/pulumi/runtime/subStack";
+
+const substack1 = new SubStack("sub1", {
+  source: "subStack",
+  prefixResourceNames: true,
+  inputs: {
+    stack: "MyStack",
+  },
+});
+
+const substack2 = new SubStack("sub2", {
+  source: "subStack",
+  prefixResourceNames: true,
+  inputs: {
+    stack: substack1.outputs.domainName,
+  },
+});
+
+export const substack2Outputs = substack2.outputs;
