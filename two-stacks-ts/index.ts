7ab2166533c84f95ce437b069bbd4a628d2280fe
Daniel Bradley <daniel@pulumi.com>
2023-11-09 17:51:28 +0000

    Switch inputs to config

--- two-stacks-ts/index.ts
+++ two-stacks-ts/index.ts
@@ -3,7 +3,7 @@ import * as pulumi from "@pulumi/pulumi";
 const sub1 = new pulumi.Program("sub1", {
   source: "../subStack",
   prefixResourceNames: true,
-  inputs: {
+  config: {
     stack: pulumi.getStack(),
   },
 });
@@ -12,11 +12,10 @@ const sub2 = new pulumi.Program("sub2", {
   source:
     "https://github.com/danielrbradley/hackathon_substack/tree/master/subStack",
   prefixResourceNames: true,
-  inputs: {
+  config: {
     stack: sub1.outputs.domainName,
   },
 });
 
 export const sub2Outputs = sub2.outputs;
-export const sub2Inputs = sub2.inputs;
 export const sub1Outputs = sub1.outputs;
